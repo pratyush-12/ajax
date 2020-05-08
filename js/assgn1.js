70825893804d8df32f408c381c72100c4341b073
pratyush-12 <36147813+pratyush-12@users.noreply.github.com>
2020-05-08 18:06:01 +0530

    added assgn files and removed all the comments

--- js/assgn1.js
+++ js/assgn1.js
@@ -11,11 +11,6 @@
                 var newitem = $("<tr> <td>"+value.name +"</td> <td>"+value.email +"</td> <td>"+value.message +"</td> <td>"+value.date +"</td> </tr>");
                 $(".detail").append(newitem);
               });
-            // setTimeout(doAjax, interval);
-      //        setTimeout(function () {
-      //   alert('Reloading Page');
-      //   location.reload(true);
-      // }, 5000);
         }
     });
        setInterval('refreshPage()', 5000);
